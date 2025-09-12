1d38db5927c2652ef9b4243916cdc4446069ecab
Leonardo Amarilio <leoamarilio1606@gmail.com>
2025-09-12 16:06:54 -0300

    Update page.tsx

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,16 +1,17 @@
 import CustomersTable from '@/app/ui/customers/table';
 import { fetchFilteredCustomers } from '@/app/lib/data';
 import { Suspense } from 'react';
+import { use } from 'react';
 
 export default async function Page({
     searchParams,
 }: {
-    searchParams?: {
+    searchParams: Promise<{
         query?: string;
         page?: string;
-    };
+    }>;
 }) {
-    const query = searchParams?.query || '';
+    const { query = '' } = use(searchParams);
     const customers = await fetchFilteredCustomers(query);
 
     return (

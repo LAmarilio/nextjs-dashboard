859d1293b905b3595e72f67ac082e1372a29234b
Leonardo Amarilio <leoamarilio1606@gmail.com>
2025-09-12 15:49:20 -0300

    Update page.tsx

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,12 @@
-export default function Page() {
-    return <p>Customers Page</p>
+import CustomersTable from '@/app/ui/customers/table';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+
+export default async function Page() {
+  const customers = await fetchFilteredCustomers('');
+
+  return (
+    <div className="p-6">
+      <CustomersTable customers={customers} />
+    </div>
+  );
 }

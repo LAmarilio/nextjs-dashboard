fc648c8cecdef7c84cfd9a29404268d34aa88f8d
Leonardo Amarilio <leoamarilio1606@gmail.com>
2025-09-12 15:52:04 -0300

    Update page.tsx

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,12 +1,15 @@
 import CustomersTable from '@/app/ui/customers/table';
 import { fetchFilteredCustomers } from '@/app/lib/data';
+import { Suspense } from 'react';
 
 export default async function Page() {
-  const customers = await fetchFilteredCustomers('');
+    const customers = await fetchFilteredCustomers('');
 
-  return (
-    <div className="p-6">
-      <CustomersTable customers={customers} />
-    </div>
-  );
+    return (
+        <div className="p-6">
+            <Suspense>
+                <CustomersTable customers={customers} />
+            </Suspense>
+        </div>
+    );
 }
